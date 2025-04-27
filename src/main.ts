96197ee69cdcb9220eedadbe221ad9a3b47e38fe
DNT <ducconit@gmail.com>
2025-04-27 23:03:29 +0700

    feat: setup i18n

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { startMockServer } from './plugins/miragejs'
+import { i18n } from './plugins/i18n'
 
 if (import.meta.env.VITE_MOCK_SERVER === 'true') {
   startMockServer()
 }
 
 const app = createApp(App)
+
+app.use(i18n)
 
 app.mount('#app')
